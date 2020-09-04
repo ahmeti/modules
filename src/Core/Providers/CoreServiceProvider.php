4c90798601e6799e8829c34176d321ecaa67e76b
Ahmet Imamoglu <ahmet363@gmail.com>
2020-09-04 19:08:40 +0300

    CoreServiceProvider tamamlandı.

---
+++ src/Core/Providers/CoreServiceProvider.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Ahmeti\Modules\Core\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('coreservice', function () {
+
+            $customCoreService = 'App\Modules\Core\Services\CoreService';
+
+            if( class_exists($customCoreService) ){
+                return new $customCoreService();
+            }
+
+            return new Ahmeti\Modules\Core\Services\CoreService;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['coreservice'];
+    }
+}
