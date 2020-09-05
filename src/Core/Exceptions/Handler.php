50a4321d7dfef7e2dfde1b122f10f8573b344145
Ahmet Imamoglu <ahmet363@gmail.com>
2020-09-05 17:06:57 +0300

    Exception Handler güncellendi.

--- src/Core/Exceptions/Handler.php
+++ src/Core/Exceptions/Handler.php
@@ -6,7 +6,6 @@ use App\Core;
 use App\Response;
 use Exception;
 use Illuminate\Database\QueryException;
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
 
@@ -30,7 +29,7 @@ class Handler
                     ->title(__('Oturum Süreniz Dolmuş'))
                     ->body(Core::alert(false, __('Oturum süreniz dolmuş. Lütfen tekrar giriş yapın.')))
                     ->jsCode('setTimeout(function(){ window.location.href = "'.url('/login?error=1').'"; }, 2000)')
-                    ->get();
+                    ->page();
             }
 
             return redirect('/login?error=1');
@@ -42,7 +41,7 @@ class Handler
                     ->message(__('Bu işlem için yetkiniz bulunmuyor.'))
                     ->title(__('Erişiminiz Engellendi'))
                     ->body(Core::alert(false, __('Bu işlem için yetkiniz bulunmuyor. Yetki almak için sistem yöneticinize başvurun.')))
-                    ->get();
+                    ->page();
             }
 
             return redirect()->route('page.error-403');
@@ -60,15 +59,15 @@ class Handler
             if( $request->wantsJson() && $request->isMethod('post') ){
                 return Response::status(false)
                     ->message(__('Bir hata oluştu ve raporlandı. En kısa sürede bu hatanın tekrar oluşmasını engelleyeceğiz.'))
-                    ->errorName('gonder')
-                    ->get();
+                    ->errorName('submit')
+                    ->page();
 
             }elseif( $request->wantsJson() && $request->isMethod('get') ){
                 return Response::status(false)
                     ->message(__('Bir hata oluştu ve raporlandı. En kısa sürede bu hatanın tekrar oluşmasını engelleyeceğiz.'))
                     ->title(__('Bir Hata Oluştu'))
                     ->body(Core::alert(false, __('Bir hata oluştu ve raporlandı. En kısa sürede bu hatanın tekrar oluşmasını engelleyeceğiz.')))
-                    ->get();
+                    ->page();
             }
         }
     }
